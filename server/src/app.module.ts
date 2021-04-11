88f365132c75c3abc9a8731dc74387096a699d8f
Mahmud <ajaomahmud@gmail.com>
2021-04-11 07:46:55 +0100

    chore: registered products module in the app root

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
