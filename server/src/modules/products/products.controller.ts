45991c5e678700d436f2ed1ee863246d8945a07a
Mahmud <ajaomahmud@gmail.com>
2021-04-14 17:09:33 +0100

    chore(Product): registered product provider to module

--- server/src/modules/products/products.controller.ts
+++ server/src/modules/products/products.controller.ts
@@ -6,11 +6,15 @@ import {
   Patch,
   Param,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { ProductQueryDto } from './dto/product-query.dto';
+import { ProductOrderDto } from './dto/product-order.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -22,13 +26,17 @@ export class ProductsController {
   }
 
   @Get()
-  async findAll(): Promise<Product[]> {
-    return this.productsService.findAll();
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Product> {
-    return this.productsService.findOne(+id);
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async findAll(
+    @Query() productOrderDto: ProductOrderDto,
+  ): Promise<{
+    total: any;
+    totalPages: number;
+    perPage: number;
+    currentPage: number;
+    items: any;
+  }> {
+    return this.productsService.findAll(productOrderDto);
   }
 
   @Patch(':id')
@@ -40,9 +48,12 @@ export class ProductsController {
   }
 
   @Get('search')
-  async search(
-    @Query('searchString') searchString: string,
-  ): Promise<Product[]> {
-    return this.productsService.search(searchString);
+  async search(@Query() productQueryDto: ProductQueryDto): Promise<Product[]> {
+    return this.productsService.search(productQueryDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Product> {
+    return this.productsService.findOne(+id);
   }
 }
--- server/src/modules/products/products.module.ts
+++ server/src/modules/products/products.module.ts
@@ -3,10 +3,11 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { productsProviders } from './products.providers';
 import { DatabaseModule } from '../../core/database/database.module';
+import { GeneralHelper } from '../../common/helpers/general-helper';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ProductsController],
-  providers: [ProductsService, ...productsProviders],
+  providers: [ProductsService, ...productsProviders, GeneralHelper],
 })
 export class ProductsModule {}
