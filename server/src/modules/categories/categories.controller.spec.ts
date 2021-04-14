b05b49ccd96f6ddd83373df5273119c421af19dc
Mahmud <ajaomahmud@gmail.com>
2021-04-14 17:13:06 +0100

    feat(Test): created categories unit tests

--- server/src/modules/categories/categories.controller.spec.ts
+++ server/src/modules/categories/categories.controller.spec.ts
@@ -1,20 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UnprocessableEntityException } from '@nestjs/common';
 
 describe('CategoriesController', () => {
-  let controller: CategoriesController;
+  let categoryController: CategoriesController;
+  const mockCategoryService = {
+    create: jest.fn((dto) => {
+      return {
+        categoryID: 1,
+        ...dto,
+      };
+    }),
+
+    findOne: jest.fn((categoryID) => {
+      return {
+        ...createCategoryDto,
+        categoryID,
+        products: [],
+      };
+    }),
+  };
+  const createCategoryDto: CreateCategoryDto = {
+    categoryName: 'Beverages',
+    picture: 'base64Image',
+    description: 'Drinks and beers',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
       providers: [CategoriesService],
-    }).compile();
+    })
+      .overrideProvider(CategoriesService)
+      .useValue(mockCategoryService)
+      .compile();
 
-    controller = module.get<CategoriesController>(CategoriesController);
+    categoryController = module.get<CategoriesController>(CategoriesController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(categoryController).toBeDefined();
+  });
+
+  it('should create a new category', async () => {
+    expect(await categoryController.create(createCategoryDto)).toEqual({
+      categoryID: expect.any(Number),
+      ...createCategoryDto,
+    });
+    expect(mockCategoryService.create).toHaveBeenCalledWith(createCategoryDto);
+  });
+
+  it('should get a category', async () => {
+    expect(await categoryController.findOne('1')).toEqual({
+      categoryID: 1,
+      ...createCategoryDto,
+      products: [],
+    });
+    expect(mockCategoryService.findOne).toBeCalled();
+  });
+
+  it('should throw an error when categoryName or picture is not provided', async () => {
+    try {
+      await categoryController.create({ picture: '', categoryName: '' });
+    } catch (e) {
+      expect(e).toBeInstanceOf(UnprocessableEntityException);
+      expect(e.message[0]).toThrowError('picture should not be empty');
+    }
   });
 });
--- server/src/modules/categories/categories.service.spec.ts
+++ server/src/modules/categories/categories.service.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { CATEGORY_REPOSITORY } from '../../core/constants';
 
 describe('CategoriesService', () => {
-  let service: CategoriesService;
+  let categoriesService: CategoriesService;
+  const createCategoryDto: CreateCategoryDto = {
+    categoryName: 'Beverages',
+    picture: 'base64Image',
+    description: 'Drinks and beers',
+  };
+  const mockCategoriesRepository = {
+    create: jest
+      .fn()
+      .mockImplementation((dto) => Promise.resolve({ categoryID: 3, ...dto })),
+
+    findOne: jest.fn().mockReturnValue({
+      ...createCategoryDto,
+      categoryID: 1,
+      products: [],
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService],
+      providers: [
+        CategoriesService,
+        {
+          provide: CATEGORY_REPOSITORY,
+          useValue: mockCategoriesRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<CategoriesService>(CategoriesService);
+    categoriesService = module.get<CategoriesService>(CategoriesService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(categoriesService).toBeDefined();
+  });
+
+  it('should create a category', async () => {
+    expect(await categoriesService.create(createCategoryDto)).toEqual({
+      categoryID: expect.any(Number),
+      ...createCategoryDto,
+    });
+    expect(mockCategoriesRepository.create).toHaveBeenCalledWith(
+      createCategoryDto,
+    );
+  });
+
+  it('should get a category', async () => {
+    expect(await categoriesService.findOne(1)).toEqual({
+      categoryID: 1,
+      ...createCategoryDto,
+      products: [],
+    });
+    expect(mockCategoriesRepository.findOne).toHaveBeenCalled();
   });
 });
