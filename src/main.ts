5f96820235fe2811743cf1a45bef0ebe82cb3eb3
Mahmud <ajaomahmud@gmail.com>
2021-04-15 20:39:04 +0100

    chore: commented out bodyparser dependency

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   // handle all user input validation globally
   app.useGlobalPipes(new ValidateInputPipe());
-  app.use(bodyParser.json({ limit: '5mb' }));
-  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+  // app.use(bodyParser.json({ limit: '5mb' }));
+  // app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
   await app.listen(process.env.PORT, () =>
     console.log(`Server running on ${process.env.PORT}`),
   );
